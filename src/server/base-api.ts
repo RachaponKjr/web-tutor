2ddfce54fd22a41b66ff224035bca717bd59c6d2
RachaponKjr <rachapon.navy@gmail.com>
2025-05-12 23:28:57 +0700

    fix

--- src/server/base-api.ts
+++ src/server/base-api.ts
@@ -6,7 +6,7 @@ import { getCookie } from "@/lib/cookie";
 // const BASE_URL: string = "http://127.0.0.1:3001";
 
 // production
-const BASE_URL: string = "http://totur-backend:3110/";
+const BASE_URL: string = "http://totur-backend:3001/";
 
 type FetchConfig = RequestInit & { headers?: Record<string, string> };
 
